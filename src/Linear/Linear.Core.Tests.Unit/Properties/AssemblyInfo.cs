162377de92c315cb31c951aecaafe37e68b4f5ba
Sotirios Mantziaris <s.mantziaris@live.com>
2015-11-20 10:58:33 +0200

    close #1

---
+++ src/Linear/Linear.Core.Tests.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Linear.Core.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bitusion")]
+[assembly: AssemblyProduct("Linear.Core.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright © Bitusion 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("69bf4553-1172-4026-ba88-1cbb8ae00a33")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Linear/Linear.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Linear.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bitusion")]
+[assembly: AssemblyProduct("Linear.Core")]
+[assembly: AssemblyCopyright("Copyright © Bitusion 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("af670d8f-d3a3-4d04-9fff-9dfe8a24bedc")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
